e24cdbf9e31627cb1f2e9d737aa4b32c4c865ed2
pgunthey327 <meshprath@gmail.com>
2026-04-05 23:48:58 +0530

    Add criteria for policy eligibility in 10000016.js
    Added criteria for policy eligibility evaluation.

--- samplerule/10000016.js
+++ samplerule/10000016.js
@@ -2,6 +2,10 @@ module.exports = {
   rulename: '10000016',
   ruleId: 'b0e4d8f9-f4bc-4ce1-c39a-e08e9f0b1c2d',
   description: 'Evaluates insurance policy eligibility for a coverage extension based on state, age, workstream, and premium amount criteria',
+  criteria: {
+    expiryDate: "01-01-2026",
+    effectiveDate: "02-03-2026",
+  },
   rule: `export function evaluatePolicy(state, age, workstream, premiumAmount) {
   const validStates = ["KA", "GJ", "RJ", "MH", "DL", "AP"];
 
