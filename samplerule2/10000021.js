56325836b03030a423c7dbc83745143e03d2f6cd
pgunthey327 <meshprath@gmail.com>
2026-04-08 00:52:28 +0530

    chore: optimised code for 10000016

--- samplerule2/10000021.js
+++ samplerule2/10000021.js
@@ -3,8 +3,9 @@ module.exports = {
   ruleId: 'd6a0f4b5-b078-4ead-ef56-ac4a5b6d7e8f',
   description: 'Calculates the Equated Monthly Instalment (EMI) for a loan given principal amount, annual interest rate, and tenure in months',
   rule: `export function calculateEMI(P, R, N) {
-  R = R / 12 / 100;
-  let emi = (P * R * Math.pow(1 + R, N)) / (Math.pow(1 + R, N) - 1);
+  const monthlyRate = R / 12 / 100;
+  const compoundFactor = Math.pow(1 + monthlyRate, N);
+  const emi = (P * monthlyRate * compoundFactor) / (compoundFactor - 1);
   return emi.toFixed(2);
 }
 console.log(calculateEMI(500000, 8, 60));`
